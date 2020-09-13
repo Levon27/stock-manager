83aff07a810b01c00d885c6d89125c64ed5ae38a
Levon Tcholakian <chahestian@hotmail.com>
2020-09-13 13:51:19 -0300

    Plot stock price in graph + favicon

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,45 @@
-function loadData(id) {
-    var ctx = document.getElementById(id).getContext('2d');
+function loadData(chart_id) {
+    var promise = fetch_data('');
+    var stock_price;
+    var stock_date;
+
+    var ctx = document.getElementById(chart_id).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
+            labels: [],
             datasets:
                 [{
                     label: 'Price',
-                    data: [0, 0.25,0.5,0.75]
+                    data: []
                 }]
         },
         options: {
         }
-    })
+    });
+
+    promise.then(response => {
+        stock_price = response.data.map(res => res.close);
+        stock_date = response.data.map(res =>new Date(res.date).getDate());
+        stock_date.reverse();
+        stock_price.reverse();
+        console.log('sucess!!',stock_price,stock_date);
+
+        myChart.data.datasets[0].data = stock_price;
+        myChart.data.labels = stock_date;
+        myChart.update();
+    });
+};
+
+function fetch_data(stock_code){ 
+    return $.ajax({
+        url: URL,
+        data: {
+            access_key: 'ff7a9dc5e3fff12d757d88cd0d795986',
+            sort: 'DESC',
+            limit: 100
+        },
+        dataType: 'json',
+    });
 }
+const URL = "http://api.marketstack.com/v1/eod?symbols=BPAC11.BVMF"
--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 const html_template =
     `<div class="card">
         <h3 class="card-title"> Cabecalho do card! </h3>
-        <canvas id="%id%"> </canvas>      
+        <canvas id="%id%"> </canvas>   
     </div>`;
 
-//04VLLDA6N8E9EQPO
+//04VLLDA6N8E9EQPO alpha vantage
+//ff7a9dc5e3fff12d757d88cd0d795986 marketstack
 
 var stocks = document.getElementById("stocks");
 
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < 1; i++) {
     var stock_card = document.createElement('div');
-    stock_card.className = 'col-lg-4';
+    stock_card.className = 'col-lg-12';
     var id_canvas = 'card'+i;
     var html = html_template.replace('%id%',id_canvas);
     stock_card.innerHTML = html;
