b14c1115e896fe8f1f603a06b737e8c714d01bc5
Levon Tcholakian <chahestian@hotmail.com>
2020-09-19 19:15:52 -0300

    Dynamic stock card html

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
-function loadData(chart_id) {
+function load_data() {
     var promise = fetch_data('');
     var stock_price;
     var stock_date;
 
-    var ctx = document.getElementById(chart_id).getContext('2d');
+    var ctx = document.getElementById("stock_price_graph").getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
@@ -20,10 +20,9 @@ function loadData(chart_id) {
 
     promise.then(response => {
         stock_price = response.data.map(res => res.close);
-        stock_date = response.data.map(res =>new Date(res.date).getDate());
+        stock_date = response.data.map(res => new Date(res.date).getDate());
         stock_date.reverse();
         stock_price.reverse();
-        console.log('sucess!!',stock_price,stock_date);
 
         myChart.data.datasets[0].data = stock_price;
         myChart.data.labels = stock_date;
@@ -31,7 +30,7 @@ function loadData(chart_id) {
     });
 };
 
-function fetch_data(stock_code){ 
+function fetch_data(stock_code) {
     return $.ajax({
         url: URL,
         data: {
@@ -42,4 +41,21 @@ function fetch_data(stock_code){
         dataType: 'json',
     });
 }
-const URL = "http://api.marketstack.com/v1/eod?symbols=BPAC11.BVMF"
+
+function create_card() {
+    var stock_card = document.createElement('div');
+    stock_card.className = 'card';
+    var id_card = 'card' + i;
+    var html = html_template.replace('%id%', id_card);
+    stock_card.innerHTML = html;
+    stocks.appendChild(stock_card);
+}
+const URL = "http://api.marketstack.com/v1/eod?symbols=BPAC11.BVMF";
+
+const html_template =     
+`
+<h5 class="card-title"> BPAC11 </h2>
+<p style="color: forestgreen;"> +100%</p>
+<p style="color: red;"> R$ 980,00</p>
+
+`
--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-const html_template =
-    `<div class="card">
-        <h3 class="card-title"> Cabecalho do card! </h3>
-        <canvas id="%id%"> </canvas>   
-    </div>`;
-
 //04VLLDA6N8E9EQPO alpha vantage
 //ff7a9dc5e3fff12d757d88cd0d795986 marketstack
 
-var stocks = document.getElementById("stocks");
+load_data();
 
-for (var i = 0; i < 1; i++) {
-    var stock_card = document.createElement('div');
-    stock_card.className = 'col-lg-12';
-    var id_canvas = 'card'+i;
-    var html = html_template.replace('%id%',id_canvas);
-    stock_card.innerHTML = html;
-    stocks.appendChild(stock_card);
-    loadData(id_canvas);
+for (var i = 0; i < 6; i++) {
+    create_card();
 }
+
