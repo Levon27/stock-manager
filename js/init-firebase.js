44f8291d154281a101e91dfe5ad4c85e2f3a926f
Levon Tcholakian <chahestian@hotmail.com>
2020-10-01 21:08:12 -0300

    Init firebase

---
+++ js/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB8WhTJmefW1Bh41QswkKv9DuO-iXEZRj8",
+    // authDomain: "project-id.firebaseapp.com",
+    // databaseURL: "https://project-id.firebaseio.com",
+    projectId: "4195777101927",
+    // storageBucket: "project-id.appspot.com",
+    // messagingSenderId: "sender-id",
+    // appID: "app-id",
+};
+
+firebase.initializeApp(firebaseConfig);
