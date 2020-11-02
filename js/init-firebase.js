51c08bc8b8423bd75aeeec04c6bb33a2e09096e3
Levon Tcholakian <chahestian@hotmail.com>
2020-11-01 21:48:55 -0300

    Get data from firestore

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -2,10 +2,11 @@ var firebaseConfig = {
     apiKey: "AIzaSyB8WhTJmefW1Bh41QswkKv9DuO-iXEZRj8",
     // authDomain: "project-id.firebaseapp.com",
     // databaseURL: "https://project-id.firebaseio.com",
-    projectId: "4195777101927",
+    projectId: "portfolio-manager-32ae7",
     // storageBucket: "project-id.appspot.com",
     // messagingSenderId: "sender-id",
     // appID: "app-id",
 };
 
-firebase.initializeApp(firebaseConfig);
+var app = firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore(app);
--- js/main.js
+++ js/main.js
@@ -2,22 +2,15 @@
 //ff7a9dc5e3fff12d757d88cd0d795986 marketstack
 
 // load_data('BPAC11');
-var i;
-const portfolio_stocks = [
-    {
-        initial_amount: 1000,
-        buy_price: 78,
-        code: 'BPAC11.BVMF',
-        
-    },
-    {
-        initial_amount: 2000,
-        buy_price: 52,
-        code: 'MGLU3.BVMF'
-    }
-]
 
-
-portfolio_stocks.forEach(stock => {
-    create_card(stock.initial_amount, stock.buy_price, stock.code);
+var i = 0;;
+db.collection('users').where('name', '==', 'jonas').get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data());
+        var portfolio_stocks = doc.data().stocks;
+        portfolio_stocks.forEach(stock => {
+            i++;
+            create_card(stock.initial_amount, stock.buy_price, stock.code);
+        });
+    });
 });
